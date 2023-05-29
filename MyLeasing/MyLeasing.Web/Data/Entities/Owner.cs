0ad681f0758dd302b20aafc3384fedaa31cd8f6e
AlexandreHenriques <xandoque@gmail.com>
2023-05-29 02:30:33 +0100

    SeedDb

--- MyLeasing/MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing/MyLeasing.Web/Data/Entities/Owner.cs
@@ -6,21 +6,27 @@ namespace MyLeasing.Web.Data.Entities
     {
         public int Id { get; set; }
 
-        [Display(Name = "Document")]
+        [Display(Name = "Document*")]
         public int Document { get; set; }
-        [Display(Name = "First Name")]
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "the field {0} can contain {1} characters length.")]
+        [Display(Name = "First Name*")]
         public string FirstName { get; set; }
-        [Display(Name = "Last Name")]
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "the field {0} can contain {1} characters length.")]
+        [Display(Name = "Last Name*")]
         public string LastName { get; set; }
 
         [Display(Name = "Fixed Phone")]
-        public string FixedPhone { get; set; }
+        public string? FixedPhone { get; set; }
 
         [Display(Name = "Cell Phone")]
-        public string CellPhone { get; set; }
+        public string? CellPhone { get; set; }
 
         [Display(Name = "Address")]
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
         [Display(Name = "Owner Name")]
         public string OwnerName { get { return $"{FirstName} {LastName}"; } }
