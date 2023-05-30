9690580ef6047b8991b9bd72a255b2026e1cd631
AlexandreHenriques <xandoque@gmail.com>
2023-05-30 04:17:59 +0100

    Repository

---
+++ MyLeasing/MyLeasing.Web/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyLeasing.Web.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+    public interface IRepository
+    {
+        void AddOwner(Owner owner);
+        void DeleteOwner(Owner owner);
+        Owner GetOwner(int id);
+        IEnumerable<Owner> GetOwners();
+        bool OwnerExists(int id);
+        Task<bool> SaveAllAsync();
+        void UpdateOwner(Owner owner);
+    }
+}
